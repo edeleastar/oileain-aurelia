ebcee77a5e50fc18ef6017438a7a772b3d494757
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-06 16:54:44 +0100

    route to poi-detail with pushState to true

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,19 @@ export class App {
 
   configureRouter(config: RouterConfiguration, router: Router) {
     config.title = "Oileain";
+    config.options.pushState = true;
     config.map([
       {
         route: [""],
         moduleId: PLATFORM.moduleName("./components/home"),
         title: "All Islands",
-      }
+      },
+      {
+        route: "poi/:id",
+        moduleId: PLATFORM.moduleName("./components/poi-detail"),
+        name: "pois",
+        title: "Island",
+      },
     ]);
   }
 }
