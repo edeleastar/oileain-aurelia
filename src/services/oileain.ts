e70f21177e7d760d4398d59719bfb28668d56389
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-06 17:23:00 +0100

    cache and index islands

--- src/services/oileain.ts
+++ src/services/oileain.ts
@@ -1,9 +1,12 @@
 import { autoinject } from "aurelia-framework";
+import { Coast, PointOfInterest, Region } from "./poi";
 import { HttpClient } from "aurelia-fetch-client";
 
 @autoinject
 export class Oileain {
   coasts: any[];
+  islandMap = new Map<string, PointOfInterest>();
+  coastMap = new Map<string, Coast>();
 
   constructor(private http: HttpClient) {}
 
@@ -11,7 +14,35 @@ export class Oileain {
     if (!this.coasts) {
       const response = await this.http.fetch("https://edeleastar.github.io/oileain-api/all-slim.json");
       this.coasts = await response.json();
+      this.createIndexes();
     }
     return this.coasts;
+  }
+
+  async getIslandById(id: string) {
+    return await this.getIsland(this.islandMap.get(id));
+  }
+
+  async getIsland(poi: PointOfInterest) {
+    let cachedPoi = this.islandMap.get(poi.safeName);
+    if (cachedPoi.description) {
+      return cachedPoi;
+    } else {
+      const path = `https://edeleastar.github.io/oileain-api/${poi.coast.variable}/${poi.safeName}.json`;
+      const response = await this.http.fetch(path);
+      const island = await response.json();
+      this.islandMap.set(poi.safeName, island);
+      return island;
+    }
+  }
+
+  createIndexes() {
+    this.coasts.forEach((coast) => {
+      this.coastMap.set(coast.variable, coast);
+      coast.pois.forEach((poi) => {
+        poi.coast = coast;
+        this.islandMap.set(poi.safeName, poi);
+      });
+    });
   }
 }
