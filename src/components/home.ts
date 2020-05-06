ca41d315267e71336cddac74e178907471ebe423
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-06 12:26:23 +0100

    display map on home component

--- src/components/home.ts
+++ src/components/home.ts
@@ -1,2 +1,19 @@
+import { LeafletMap } from "services/leaflet-map";
+
 export class Home {
+
+  mapDescriptor = {
+    id: "home-map-id",
+    height: 1200,
+    location: { lat: 53.2734, long: -7.7783203 },
+    zoom: 8,
+    minZoom: 7,
+    activeLayer: "",
+  };
+
+  map: LeafletMap;
+
+  attached() {
+    this.map = new LeafletMap(this.mapDescriptor);
+  }
 }
