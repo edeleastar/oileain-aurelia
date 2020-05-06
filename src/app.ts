4994a99274780f05ba76ebc9e25fa2da7c7950f5
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-06 08:55:28 +0100

    incorporate aurelia-fetch-client, uikit + fontawesome

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,3 @@
 export class App {
-  public message: string = 'Hello World!';
+  public message: string = "Hello World!";
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import {Aurelia} from 'aurelia-framework';
-import * as environment from '../config/environment.json';
-import {PLATFORM} from 'aurelia-pal';
+import { Aurelia } from "aurelia-framework";
+import * as environment from "../config/environment.json";
+import { PLATFORM } from "aurelia-pal";
+
+import "uikit/dist/css/uikit.css";
+import "uikit/dist/js/uikit.js";
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    .plugin(PLATFORM.moduleName("aurelia-fontawesome"))
+    .feature(PLATFORM.moduleName("resources/index"));
 
-  aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
+  aurelia.use.developmentLogging(environment.debug ? "debug" : "warn");
 
   if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
+    aurelia.use.plugin(PLATFORM.moduleName("aurelia-testing"));
   }
 
-  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
+  aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName("app")));
 }
