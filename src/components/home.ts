f0533df27489766c6bc03d717a8d84212b6e48c4
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-06 16:08:32 +0100

    populate costal-map from service

--- src/components/home.ts
+++ src/components/home.ts
@@ -1,7 +1,10 @@
-import { LeafletMap } from "services/leaflet-map";
+import { autoinject } from "aurelia-framework";
+import { CoastalLeafletMap } from "services/coastal-leaflet-map";
+import { Coast } from "services/poi";
+import { Oileain } from "services/oileain";
 
+@autoinject
 export class Home {
-
   mapDescriptor = {
     id: "home-map-id",
     height: 1200,
@@ -11,9 +14,19 @@ export class Home {
     activeLayer: "",
   };
 
-  map: LeafletMap;
+  map: CoastalLeafletMap;
+  coasts: Array<Coast>;
+
+  constructor(private oileain: Oileain) {}
+
+  async activate(params: any) {
+    this.coasts = await this.oileain.getCoasts();
+  }
 
   attached() {
-    this.map = new LeafletMap(this.mapDescriptor);
+    this.map = new CoastalLeafletMap(this.mapDescriptor);
+    if (this.coasts) {
+      this.map.populateCoasts(this.coasts);
+    }
   }
 }
