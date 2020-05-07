80a833f6cdfbfdedfd2598e2ae66ff5784dfc313
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-07 10:54:32 +0100

    link to sidebar from header

--- src/resources/elements/header.ts
+++ src/resources/elements/header.ts
@@ -1,9 +1,10 @@
 import { bindable } from "aurelia-framework";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";;
-import { faMapMarked } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMapMarked } from "@fortawesome/free-solid-svg-icons";
 
 export class Header {
   @bindable title: string;
   github = faGithub;
   map = faMapMarked;
+  bars = faBars;
 }
