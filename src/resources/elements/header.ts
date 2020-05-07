537f213d1cf44c9eae6f576b62edeea4968c97f9
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-07 11:36:58 +0100

    icon for navigator

--- src/resources/elements/header.ts
+++ src/resources/elements/header.ts
@@ -1,10 +1,12 @@
 import { bindable } from "aurelia-framework";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";;
 import { faBars, faMapMarked } from "@fortawesome/free-solid-svg-icons";
+import { faMap } from "@fortawesome/free-regular-svg-icons";
 
 export class Header {
   @bindable title: string;
   github = faGithub;
   map = faMapMarked;
+  mapAlt = faMap;
   bars = faBars;
 }
