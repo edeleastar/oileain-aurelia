9b54357de93442c91fdbf4979af10e1ff0189c38
Eamonn de Leastar <edeleastar@wit.ie>
2020-05-07 10:34:27 +0100

    main maps button on title bar

--- src/resources/elements/header.ts
+++ src/resources/elements/header.ts
@@ -1,7 +1,9 @@
 import { bindable } from "aurelia-framework";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";;
+import { faMapMarked } from "@fortawesome/free-solid-svg-icons";
 
 export class Header {
   @bindable title: string;
   github = faGithub;
+  map = faMapMarked;
 }
